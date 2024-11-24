07c2b72dc71ae433856018c8a182096d78a87cc6
Mal'Dun Finsterschreck <Stefan Reiterer>
2024-11-24 19:01:47 +0100

    updated to gtd

--- .scripts/merge_script.py
+++ .scripts/merge_script.py
@@ -10,15 +10,15 @@ import shutil
 HOME = os.path.expanduser("~/")
 sys.path.append(HOME + "prog/Python/hoi4_converter/")
 
-HOI4_FOLDER = HOME + ".local/share/Steam/steamapps/common/Hearts of Iron IV/"
-KR_FOLDER = HOME + ".local/share/Steam/steamapps/workshop/content/394360/1521695605/"
-KX_FOLDER = HOME + ".local/share/Steam/steamapps/workshop/content/394360/2076426030/"
-# RT56_FOLDER = HOME + ".local/share/Steam/steamapps/workshop/content/394360/820260968/"
+HOI4_FOLDER = HOME + "SSD/SteamLibrary/steamapps/common/Hearts of Iron IV/"
+KR_FOLDER = HOME + "SSD/SteamLibrary/steamapps/workshop/content/394360/1521695605/"
+KX_FOLDER = HOME + "SSD/SteamLibrary/steamapps/workshop/content/394360/2076426030/"
+# RT56_FOLDER = HOME + "SSD/SteamLibrary/steamapps/workshop/content/394360/820260968/"
 RT56_FOLDER = HOME + ".local/share/Paradox Interactive/Hearts of Iron IV/mod/1956_beta/"
 KR_NAVAL_FOLDER = HOME + \
-    ".local/share/Steam/steamapps/workshop/content/394360/2862849828/"
+    "SSD/SteamLibrary/steamapps/workshop/content/394360/2862849828/"
 KX_NAVAL_FOLDER = HOME + \
-    ".local/share/Steam/steamapps/workshop/content/394360/2964793578/"
+    "SSD/SteamLibrary/steamapps/workshop/content/394360/2964793578/"
 
 # Set for mod in question
 KX = True
--- descriptor.mod
+++ descriptor.mod
@@ -4,12 +4,12 @@ tags={
 	"National Focuses"
 }
 dependencies={
-	"Kaiserredux: A Kaiserreich Expansion"
+	"KaiserreduX"
 	"Autobahn nach 56 Redux"
 	"Kaiserredux Naval Rework"
 }
 name="Autobahn nach 56 and Kaiserredux Naval Rework Compatch"
 picture="thumbnail.png"
-supported_version="1.12.*"
+supported_version="1.13*"
 path="/home/maldun/.local/share/Paradox Interactive/Hearts of Iron IV/mod/autobahn_kx_naval_compatch"
 remote_file_id="2990840390"
